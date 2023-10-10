08482cee7c5387f2febf38f5fce0b5eb5124f223
DonAllenII <don.a.alexanderii@gmail.com>
2023-10-10 14:06:55 -0400

    added house functions module

---
+++ house_mod.py
@@ -0,0 +1,9 @@
+from math import pi
+
+def house_area(length, width):
+    area = length * width
+    return area
+
+def circumference (radius):
+    circumference = 2 * pi * radius
+    return circumference
